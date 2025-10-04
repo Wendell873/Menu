efcef359245e260c08641535ba60aa86bad80f9a
Wendell Lima <wendelllimasilva873@gmail.com>
2025-10-04 00:29:19 -0300

    Menuzinho3

---
+++ app/src/main/java/com/wendell/navigation/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.Wendell.navigation
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.Wendell.navigation.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.wendell.navigation.NotebooksFragment
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val fragmentManager: FragmentManager by lazy { supportFragmentManager }
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Ocultar a status bar
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView)
+            .hide(WindowInsetsCompat.Type.statusBars())
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        // BottomNavigation
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_cart -> openFragment(CarrinhoFragment())
+                R.id.bottom_profile -> openFragment(PerfilFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias", Toast.LENGTH_SHORT).show()
+        }
+
+        // Back button
+        onBackPressedDispatcher.addCallback(this) {
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                finish()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_Computadores -> openFragment(ComputadoresFragment())
+            R.id.nav_Celulares -> openFragment(CelularesFragment())
+            R.id.nav_Notebooks -> openFragment(NotebooksFragment())
+            R.id.nav_Fones -> openFragment(FonesFragment())
+            R.id.nav_Tablets -> openFragment(TabletsFragment())
+            R.id.nav_Carregadores -> openFragment(CarregadoresFragment())
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
